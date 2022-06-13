9c620f34cb012a162da885023c6ee1b7872f55cf
Nahyan Sharvin <nskonnalath@gmail.com>
2022-06-13 22:39:34 +0530

    add typical in home page

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Typical from 'react-typical'
 import Hero from './hero_image.svg'
 //Custom Components
 import {CtaButton} from '../../components'
@@ -13,7 +14,7 @@ function Home() {
             </div>
             <div className='hero-text'>
                 <h1>Welcome to the </h1><h1>Home Page</h1>
-                <p>This is the home page of the application</p>
+                <Typical loop={Infinity} steps={["This is the home page of the application"]} />
                 <CtaButton styleName='primary-btn' text="Know More" />
             </div>
           </div>
