95856212d36dd069c2bbc8da8d18211cdaa920bc
Nahyan Sharvin <nskonnalath@gmail.com>
2022-06-15 00:38:03 +0530

    set background animation in vision section

--- src/containers/vision/Vision.js
+++ src/containers/vision/Vision.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Vision.css'
 import { Heading } from '../../components'
+import { useState } from 'react'
 
 function Vision() {
+  const [animation, setAnimation] = useState(false)
+
+  
   return (
-    <div className="vision-wrapper" id='vision'>
+    <div className={`vision-wrapper ${animation ? "gradient-animation":""}`} id='vision'>
       <div className="vision-heading-container">  <Heading title="Vision" subtitle="Our vision" />
       </div>
       <div className="vision-container section-margin">
-        <div className="vision-text body-text">
+        <div className={`vision-text body-text ${animation ? "gradient-text":""}`}>
           <h4>Lorem Ipsum is simply dummy text for printing</h4>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <button onClick={() => setAnimation(!animation)}>See the Magic👻</button>
         </div>
         <div className="vision-image">
           <img src={require('./vision.svg').default} alt="vision"/>
