8397727e6b687484c18a980b5dd00bafab081826
Nahyan Sharvin <nskonnalath@gmail.com>
2022-06-17 11:28:56 +0530

    logout button fixed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,17 +5,18 @@ import { useState } from 'react';
 //Icons
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import PersonIcon from '@mui/icons-material/Person';
 
 //Custom Components
-import {Login,Signup} from '../../components'
+import { Login, Signup } from '../../components'
 
 const NavMenu = () => (
   <>
-  <p><a href='#home'>Home</a></p>
-  <p><a href='#about'>About</a></p>
-  <p><a href='#tech'>Tech</a></p>
-  <p><a href='#vision'>Vision</a></p>
-  <p><a href='#contact'>Contact</a></p>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#about'>About</a></p>
+    <p><a href='#tech'>Tech</a></p>
+    <p><a href='#vision'>Vision</a></p>
+    <p><a href='#contact'>Contact</a></p>
   </>
 )
 
@@ -26,38 +27,44 @@ function Navbar() {
   const [toggleLogin, setToggleLogin] = useState(false)
   const [toggleSignupForm, setToggleSignupForm] = useState(false)
   const [loggedIn, setLoggedIn] = useState(false)
-  const [users, setUsers] = useState([{username: 'user', password: 'user'}])
+  const [users, setUsers] = useState([{ username: 'user', password: 'user' }])
 
   return (
-      <>
-        <nav className="navbar">
-          <div className="navbar-container">
-            <div className="navbar-logo">
-              LOGO
-            </div> 
-            <div className="navbar-menu">
-              <NavMenu/>
-              <div className="login-button-container">
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-logo">
+            LOGO
+          </div>
+          <div className="navbar-menu">
+            <NavMenu />
+            <div className="login-button-container">
+              {loggedIn ?
+                <div className='profile-icon-container'>
+                  <PersonIcon fontSize='large' />
+                  <a onClick={() => setLoggedIn(false)}>Logout</a>
+                </div> :
                 <button className="login-button" onClick={() => setToggleLogin(true)}>Login</button>
-              </div>
+              }
             </div>
-            <div className="navbar-menu-toggle">
-            {toggleMenu ? <CloseIcon fontSize="large" onClick={() => setToggleMenu(false)}/> : <MenuIcon fontSize='large' onClick={() => setToggleMenu(true)}/>}
-            {toggleMenu &&(
+          </div>
+          <div className="navbar-menu-toggle">
+            {toggleMenu ? <CloseIcon fontSize="large" onClick={() => setToggleMenu(false)} /> : <MenuIcon fontSize='large' onClick={() => setToggleMenu(true)} />}
+            {toggleMenu && (
               <div className="navbar-menu-container animate__animated animate__fadeInDown">
-                <NavMenu/>
+                <NavMenu />
               </div>
             )}
           </div>
+        </div>
+        {toggleLogin && (
+          <div className="navbar-login-container">
+            {toggleSignupForm ? <Signup users={users} setUsers={setUsers} setToggleSignupForm={setToggleSignupForm} setToggleLogin={setToggleLogin} /> : <Login users={users} setLoggedIn={setLoggedIn} setToggleSignupForm={setToggleSignupForm} setToggleLogin={setToggleLogin} />}
+
           </div>
-          {toggleLogin &&(
-            <div className="navbar-login-container">
-            {toggleSignupForm ? <Signup users={users} setUsers={setUsers} setToggleSignupForm={setToggleSignupForm} setToggleLogin={setToggleLogin} /> : <Login users={users} loggedIn={loggedIn} setLoggedIn={setLoggedIn} setToggleSignupForm={setToggleSignupForm} setToggleLogin={setToggleLogin} />}
-            
-          </div>
-          )}
-        </nav>
-      </>
+        )}
+      </nav>
+    </>
   )
 }
 
