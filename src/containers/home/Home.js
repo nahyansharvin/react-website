49d49646558569f06a877263f52848500d3bf341
Nahyan Sharvin <nskonnalath@gmail.com>
2022-06-17 11:52:55 +0530

    remove react-typical

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Home.css'
-import Typical from 'react-typical'
 import Hero from './hero_image.svg'
 //Custom Components
 import {CtaButton} from '../../components'
@@ -14,7 +13,6 @@ function Home() {
             </div>
             <div className='hero-text'>
                 <h1>Welcome to the </h1><h1>Home Page</h1>
-                {/* <Typical loop={Infinity} steps={["This is the home page of the application"]} /> */}
                 <p>This is the home page of the application</p>
                 <CtaButton text="Know More" target="#about" />
             </div>
