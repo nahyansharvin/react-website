bdac30e035d5c239c90f5e1fe84de1b6844823ff
Nahyan Sharvin <nskonnalath@gmail.com>
2022-06-17 12:02:56 +0530

    warning fix

--- src/components/socialmedia/SocialMedia.js
+++ src/components/socialmedia/SocialMedia.js
@@ -12,10 +12,10 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 function Socialmedia() {
   return (
     <div className='socialmedia'>
-    <div className='socialmedia-icon' ><a href='https://www.linkedin.com/in/nahyansharvin/' target="_blank" ><LinkedInIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
-    <div className='socialmedia-icon' ><a href='https://github.com/nahyansharvin' target="_blank" ><GitHubIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
-    <div className='socialmedia-icon' ><a href='https://www.facebook.com/nahyan.konnalath/' target="_blank" ><FacebookIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
-    <div className='socialmedia-icon' ><a href='https://www.instagram.com/_nhyn.__/' target="_blank" ><InstagramIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
+    <div className='socialmedia-icon' ><a href='https://www.linkedin.com/in/nahyansharvin/' target="_blank" rel='noreferrer' ><LinkedInIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
+    <div className='socialmedia-icon' ><a href='https://github.com/nahyansharvin' target="_blank" rel='noreferrer' ><GitHubIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
+    <div className='socialmedia-icon' ><a href='https://www.facebook.com/nahyan.konnalath/' target="_blank" rel='noreferrer' ><FacebookIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
+    <div className='socialmedia-icon' ><a href='https://www.instagram.com/_nhyn.__/' target="_blank" rel='noreferrer' ><InstagramIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
 </div>
   )
 }
